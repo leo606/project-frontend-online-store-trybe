33b3e4d6b529c7279979b533f0b4d9dfbb2e6e8f
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-15 18:34:05 -0300

    Updates

---
+++ precache-manifest.8d7158d06fc0e0979b483efd03582c97.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903089c572b0d0e59aa20edc42a6c086",
+    "url": "/project-frontend-online-store-trybe/index.html"
+  },
+  {
+    "revision": "2f1a269e045385e4046d",
+    "url": "/project-frontend-online-store-trybe/static/css/main.496fcf31.chunk.css"
+  },
+  {
+    "revision": "c3d3cc5d0e98ff0f399b",
+    "url": "/project-frontend-online-store-trybe/static/js/2.4777a782.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/project-frontend-online-store-trybe/static/js/2.4777a782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1a269e045385e4046d",
+    "url": "/project-frontend-online-store-trybe/static/js/main.7e6caef3.chunk.js"
+  },
+  {
+    "revision": "e6d8eb92ae0702780316",
+    "url": "/project-frontend-online-store-trybe/static/js/runtime-main.b6232d3f.js"
+  },
+  {
+    "revision": "64a255b0f9ac36d7385fe4ecca35478e",
+    "url": "/project-frontend-online-store-trybe/static/media/LandasansMedium-ALJ6m.64a255b0.otf"
+  },
+  {
+    "revision": "89abd579fe417dc4397bdb09b8e88cad",
+    "url": "/project-frontend-online-store-trybe/static/media/TipoPress-DgaW.89abd579.ttf"
+  },
+  {
+    "revision": "9217ca3896a40b661d65fe3a3603d683",
+    "url": "/project-frontend-online-store-trybe/static/media/elo.9217ca38.png"
+  },
+  {
+    "revision": "aee1c181892814a8352e03fd4b0e0207",
+    "url": "/project-frontend-online-store-trybe/static/media/faustao-errou.aee1c181.gif"
+  },
+  {
+    "revision": "545819e1ef7017a8b514847eab90ba37",
+    "url": "/project-frontend-online-store-trybe/static/media/faustao.545819e1.gif"
+  },
+  {
+    "revision": "4c4f5d3be13c4964d29c681164ff8a21",
+    "url": "/project-frontend-online-store-trybe/static/media/master.4c4f5d3b.png"
+  },
+  {
+    "revision": "2c23636a3472c42be89f7aedf221d070",
+    "url": "/project-frontend-online-store-trybe/static/media/se_vira_logo.2c23636a.png"
+  },
+  {
+    "revision": "f7d8cd98636377b4cbce9c6c69ac5dea",
+    "url": "/project-frontend-online-store-trybe/static/media/visa.f7d8cd98.png"
+  }
+]);
